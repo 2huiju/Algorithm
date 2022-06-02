abe5e02ead541830990343e7a4546a557b8f97ef
heejoo45890 <heejoo45890@gmail.com>
2022-06-02 18:17:26 +0900

    0602

---
+++ 4주차/화요일/제일 작은 수 제거하기.js
@@ -0,0 +1,56 @@
+// 제일 작은 수 제거하기
+
+// 문제 설명
+// 정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요. 
+// 단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요. 예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
+
+// 제한 조건
+// arr은 길이 1 이상인 배열입니다.
+// 인덱스 i, j에 대해 i ≠ j이면 arr[i] ≠ arr[j] 입니다.
+
+// 입출력 예
+// arr	return
+// [4,3,2,1]	[4,3,2]
+// [10]	[-1]
+
+// 내가 푼 거
+function solution(arr) {
+    let dd = []
+    if (arr.length !== 1) {
+      dd = [...arr].sort((a,b) => b-a).pop()
+      let answer = arr.filter((i) => i !== dd)
+      return answer
+    } else return [-1]
+  }
+
+// reference1
+function solution(arr) {
+  const answer = []
+  
+  //1.제일 작은 수 찾기
+  let min = arr[0]
+  //가장 작은 값을 저장하는 변수
+  
+  for(let i = 1; i<arr.length; i++) {
+      if(arr[i] < min) {
+          min = arr[i]
+      }
+  }
+    
+    //2. 제일 작은 수를 배열에서 제거 (제일 작은 수를 제외한 나머지 숫자를 새로운 배열에 넣는다)
+    for(let i = 0; i<arr.length; i++) {
+        if(arr[i] !== min) {
+            answer.push(arr[i])
+        }
+    }
+    return answer.length === 0 ? [-1] : answer
+}
+
+// reference2
+function solution(arr) {
+    
+  const min = Math.min(...arr)
+  let answer = arr.filter((i) => i !== min)
+
+  return answer.length ===0 ? [-1] : answer
+}
