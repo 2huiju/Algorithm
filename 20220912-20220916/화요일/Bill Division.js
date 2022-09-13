0f27129e4649c83378047cbe8f3f8b5c4ed3c44a
heejoo45890 <heejoo45890@gmail.com>
2022-09-13 18:35:04 +0900

    0913

--- 20220912-20220916/화요일/Bill Division.js
+++ 20220912-20220916/화요일/Bill Division.js
@@ -5,3 +5,14 @@ function bonAppetit(bill, k, b) {
   if (money === b) console.log("Bon Appetit");
   else console.log(b - money);
 }
+
+// 처음엔 이렇게 풀었는데 filter 때문에 배열 순회하는데 오래걸려서 인풋값이 몇천개일 경우 시간초과가 남
+// 그래서 splice 로 변경해줌
+function bonAppetit(bill, k, b) {
+  // Write your code here
+  let money = bill.filter((el) => el !== bill[k]).reduce((a, b) => a + b) / 2;
+
+  if (money === b) return "Bon Appetit";
+  else return b - money;
+}
+bonAppetit([3, 10, 2, 9], 1, 12);
